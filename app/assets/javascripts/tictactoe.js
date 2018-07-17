17d27d5f5a605e7707c84c1be37fd31dc9dfc1c8
Sean Oughton <seanoughton@gmail.com>
2018-07-17 20:38:01 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -41,8 +41,12 @@ function setMessage(string) {
 //Returns true if the current board contains any winning combinations (three X or O tokens in a row, vertically, horizontally, or diagonally). Otherwise, returns false.
 //If there is a winning combination on the board, checkWinner() should invoke setMessage(), passing in the appropriate string based on who won: 'Player X Won!' or 'Player O Won!'
 function checkWinner() {
-  
-  
+  //has to get the current state of the board
+  //iterate through board using jquery to grab the html inside each td and create an array of those values
+  //compare the board array to the winning combinations
+
+
+
   var message = "Player ${winner} Won!"
   setMessage(message);
   return // true or false, depending on who won
