11fcb321401dd17103909a45e0deff3a5083694e
Sean Oughton <seanoughton@gmail.com>
2018-07-17 20:26:39 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -5,6 +5,17 @@ $( document ).ready(function() {
 
 var turn = 1;
 
+const WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+];
+
 //Returns the token of the player whose turn it is, 'X' when the turn variable is even and 'O' when it is odd.
 function player() {
   var player = ""
@@ -25,4 +36,14 @@ function updateState(square) {
 //Accepts a string and adds it to the div#message element in the DOM.
 function setMessage(string) {
   $( "#message" ).html(string);
+}
+
+//Returns true if the current board contains any winning combinations (three X or O tokens in a row, vertically, horizontally, or diagonally). Otherwise, returns false.
+//If there is a winning combination on the board, checkWinner() should invoke setMessage(), passing in the appropriate string based on who won: 'Player X Won!' or 'Player O Won!'
+function checkWinner() {
+  
+  
+  var message = "Player ${winner} Won!"
+  setMessage(message);
+  return // true or false, depending on who won
 }
