7feafb1ad3e895f246e44e7db58f8b20f3d53b2c
Sean Oughton <seanoughton@gmail.com>
2018-07-18 21:37:30 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,13 @@
 // Code your JavaScript / jQuery solution here
 $( document ).ready(function() {
 
+//Attaches the appropriate event listeners to the squares of the game board as well as for the button#save, button#previous, and button#clear elements.
+//When a user clicks on a square on the game board, the event listener should invoke doTurn() and pass it the element that was clicked.
+//When you name your save and previous functions, make sure to call them something like saveGame() and previousGames(). If you call them save() and previous() you may run into problems with the test suite.
+  function attachListeners(){
+    
+  };
+
 });//end document.ready
 
 var turn = 1;
@@ -116,3 +123,5 @@ function doTurn(square) {
     resetBoard();
   };
 };
+
+
