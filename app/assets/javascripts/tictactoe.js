a78c759fa12feb442ac074a631eb967698eefb32
Sean Oughton <seanoughton@gmail.com>
2018-07-19 16:18:33 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -18,6 +18,12 @@ function attachListeners(){
 
 };
 
+<<<<<<< HEAD
+=======
+//Attaches the appropriate event listeners to the squares of the game board as well as for the button#save, button#previous, and button#clear elements.
+//When a user clicks on a square on the game board, the event listener should invoke doTurn() and pass it the element that was clicked.
+//When you name your save and previous functions, make sure to call them something like saveGame() and previousGames(). If you call them save() and previous() you may run into problems with the test suite.
+>>>>>>> 99c974872b85637f6637295f64280eacaff571f0
 
 
 
